c9e0841501153f31cf59f8787d57d9a5b0238a62
RodrigoRiE <88850926+RodrigoRiE@users.noreply.github.com>
2021-08-23 23:49:13 -0500

    Update exercise.py

--- assignments/09Telefono/src/exercise.py
+++ assignments/09Telefono/src/exercise.py
@@ -1,6 +1,15 @@
 def main():
     #escribe tu código abajo de esta línea
-    #Leer los datos
+    def calc_cmensual(men,meg,tiemp):
+        suma = men + meg + tiemp
+        total = suma*0.80
+        return total
+
+    men = float(input("Give me the maessajes: "))
+    meg = float(input("Give me the megas: "))
+    tiemp = float(input("Give me the minutes: "))
+
+    print("The monthly cost is:",calc_cmensual(men,meg,tiemp))
     pass
 
 
