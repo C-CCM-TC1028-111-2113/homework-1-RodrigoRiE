848e05274241e61fcc1c0cf2cc73c84e69f154c4
RodrigoRiE <88850926+RodrigoRiE@users.noreply.github.com>
2021-08-23 23:47:36 -0500

    Update exercise.py

--- assignments/08Caracol/src/exercise.py
+++ assignments/08Caracol/src/exercise.py
@@ -1,5 +1,12 @@
 def main():
     #escribe tu código abajo de esta línea
+    def caracol (minu):
+        seg = minu * 60
+        car = 0.57 * seg
+        return car
+
+    minu = int(input("Give me the minutes: "))
+    print("The distance traveled by the snail: ",caracol(minu))
     pass
 
 if __name__ == '__main__':
