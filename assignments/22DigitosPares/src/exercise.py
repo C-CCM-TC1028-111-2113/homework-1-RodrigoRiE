7bb7152a267d00dfa1dcd262e29522d40effefcb
RodrigoRiE <88850926+RodrigoRiE@users.noreply.github.com>
2021-08-23 23:53:21 -0500

    Update exercise.py

--- assignments/22DigitosPares/src/exercise.py
+++ assignments/22DigitosPares/src/exercise.py
@@ -1,5 +1,18 @@
 def main():
     #escribe tu código abajo de esta línea
+    def pares_impares(number):
+      even_num = 0
+      odd_num = 0
+      while(number > 0):
+        if number % 2 == 0:
+          even_num += 1
+        else:
+          odd_num += 1
+        number = number // 10
+      return odd_num
+ 
+    num = int(input("Enter your number: "))
+    print("Pares: ",pares_impares(num))
     pass
 
 
